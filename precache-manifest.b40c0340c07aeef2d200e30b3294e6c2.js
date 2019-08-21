93334a9b139d3907be160f3604504a7378a03423
GhabiHamza <ghabihamza77@gmail.com>
2019-08-21 13:26:52 +0200

    Updates

---
+++ precache-manifest.b40c0340c07aeef2d200e30b3294e6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89162aa87ba2f9e7efc11e3f1df29076",
+    "url": "/Credit%20Card/index.html"
+  },
+  {
+    "revision": "764b81b4ae42438032a5",
+    "url": "/Credit%20Card/static/css/main.93f73d07.chunk.css"
+  },
+  {
+    "revision": "6813cc6da8a8a51c0880",
+    "url": "/Credit%20Card/static/js/2.1eeb86ee.chunk.js"
+  },
+  {
+    "revision": "764b81b4ae42438032a5",
+    "url": "/Credit%20Card/static/js/main.05d70f80.chunk.js"
+  },
+  {
+    "revision": "85b3f21e4d7e9ea11ea3",
+    "url": "/Credit%20Card/static/js/runtime~main.82dcbaed.js"
+  },
+  {
+    "revision": "a348e897fd16171c65e24bc816d5a792",
+    "url": "/Credit%20Card/static/media/images.a348e897.jpg"
+  }
+]);
